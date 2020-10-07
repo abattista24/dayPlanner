eea3a189c57eb128b8ac720b6b52cb3e6c556564
Anna Battistals <anna.r.battista@gmail.com>
2020-10-06 23:52:47 -0600

    fixed issues with local storage and added time colors

--- assets/script.js
+++ assets/script.js
@@ -5,28 +5,36 @@ var dateEl =(".todayIs");
 var todoInput =$(".todo").value;
 //document.querySelector(".todo".value);
 
-console.log(todoInput) ;
+console.log(todoInput);
 $(dateEl).text(currentDate);
 
 // planner app knows the current day 
     function clock (){
-        var timeEl = currentDate.update
-        var blockTime = ("h3").text.parseint
-        if (blockTime === timeEl){
-            (".this").background(purple);
+        var currentTime = moment().hours();
+        console.log (currentTime)
+        $(".bodyRow").each(function(){
+            var timeBlock = $(this).attr("id");
+            console.log(timeBlock);
+            if (timeBlock < currentTime){
+                $(this).attr("class","row bodyRow past");
+                //change to past color  
+    
+            }else if (timeBlock > currentTime) {
+                $(this).attr("class","row bodyRow future");
+                //change to future color
+            
+            }else {
+               $(this).attr("class","row bodyRow present");
+            //change to current color  
+            } 
 
-
-        }else if (blockTime > timeEl) {
-            (".this").background(greem);
-        //change to future color
-        
-        }else if (blockTime < TimeEl){
-            (".this").background(blue);
-        //change to past color  
-        }; 
+        });
+        // var timeEl = currentDate.update
+        // var blockTime = ("h3").text.parseint
+       
     };
 
-
+clock();
          
           
     
@@ -35,21 +43,32 @@ $(dateEl).text(currentDate);
         $(".saveBtn").on("click", function(e){
             e.preventDefault();
             console.log("click");
+            var inputValue = $(this).siblings(".description").val(); 
+            var time = $(this).parent().attr("id")
+
         //   var userString = JSON.stringify(todoInput);
             // var userString = todoInput.text
-            localStorage.setItem(".input", todoInput);
+            localStorage.setItem(time,inputValue);
 
          //display saved content onto page
 
-            var todoSave = localStorage.setItem(".input", todoInput);
-            $(".this").textContent.todoSave;
+            // var todoSave = localStorage.setItem(".input", todoInput);
+            // $(".this").textContent.todoSave;
 
     
 
         });
    
-
-        
+        $("#8 .description").val(localStorage.getItem("8"));
+        $("#9 .description").val(localStorage.getItem("9"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#13 .description").val(localStorage.getItem("13"));
+        $("#14 .description").val(localStorage.getItem("14"))
+        $("#15 .description").val(localStorage.getItem("15")) 
+        $("#16 .description").val(localStorage.getItem("16")) 
+        $("#17 .description").val(localStorage.getItem("17"))
        
 });
 
